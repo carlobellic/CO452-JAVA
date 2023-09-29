9535fe71fd36caacbd4e1584fd766975fa8823de
100185948 <100185948@172.18.4.134>
2023-09-29 14:43:25 +0100

    new code

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -2,7 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+
 package javaapplication10;
+
+import helpers.*;
+
 
 /**
  *
@@ -15,6 +20,11 @@ public class JavaApplication10 {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        String name;
+        name = InputReader.getString("Hello, what’s your name?"); 
+        System.out.println("Hello " + name + "!");
+
+
     }
     
 }
