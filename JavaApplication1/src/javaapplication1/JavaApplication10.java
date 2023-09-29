0b2e971bc080bfdf3a8fd2585b6315cdb079f1f1
100185948 <100185948@172.18.4.134>
2023-09-29 14:20:27 +0100

    new code

---
+++ JavaApplication1/src/javaapplication1/JavaApplication10.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 100185948
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in); 
+        String userName;
+        
+        System.out.println("Enter username");
+        userName = myObj.nextline();
+        
+        System.out.println("Username is:" + userName);
+        
+    }
+}
+        
+        
+    
