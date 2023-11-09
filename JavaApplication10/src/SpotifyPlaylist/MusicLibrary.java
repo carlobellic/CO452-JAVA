a343bc5fa46ae4b2148247187e857f11814b10f9
carlobellic <145754932+carlobellic@users.noreply.github.com>
2023-11-09 01:03:38 +0000

    Update MusicLibrary.java
    Added comments to the code.

--- JavaApplication10/src/SpotifyPlaylist/MusicLibrary.java
+++ JavaApplication10/src/SpotifyPlaylist/MusicLibrary.java
@@ -2,23 +2,29 @@ package SpotifyPlaylist;
 
 import java.util.List;
 import java.util.ArrayList;
+
 public class MusicLibrary {
+    // Private list to store songs in the music library
     private List<Song> songs = new ArrayList<>();
 
+    // Method to add a song to the music library
     public void addSong(Song song) {
         songs.add(song);
     }
 
+    // Method to remove a song from the music library
     public void removeSong(Song song) {
         songs.remove(song);
     }
 
+    // Method to print all songs in the music library
     public void printAllSongs() {
         for (Song song : songs) {
             System.out.println(song);
         }
     }
 
+    // Method to print songs with a play count greater than a specified minimum play count
     public void printSongsOverPlayCount(int minPlayCount) {
         for (Song song : songs) {
             if (song.getPlayCount() > minPlayCount) {
