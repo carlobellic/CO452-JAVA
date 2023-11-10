f18a7ca53c0abc4d61ab930c08f5eaafe8b0e892
carlobellic <145754932+carlobellic@users.noreply.github.com>
2023-11-10 09:27:56 +0000

    Added comments to song class.

--- JavaApplication10/src/SpotifyPlaylist/Song.java
+++ JavaApplication10/src/SpotifyPlaylist/Song.java
@@ -4,30 +4,31 @@ class Song {
     private String title;
     private String artist;
     private int playCount;
-
+    // used to print out the song list with different statistics.
     public Song(String title, String artist, int playCount) {
         this.title = title;
         this.artist = artist;
         this.playCount = playCount;
     }
-
+    // gets the title of the song.
     public String getTitle() {
         return title;
     }
-
+    // gets the artists name.
     public String getArtist() {
         return artist;
     }
-
+    // gets the songs play count.
     public int getPlayCount() {
         return playCount;
     }
-
+ 
     public void incrementPlayCount() {
         playCount++;
     }
-
+     
     @Override
+    // returns the artist and play count.
     public String toString() {
         return title + " by " + artist + " (Play Count: " + playCount + ")";
     }
